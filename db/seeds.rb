94ae5983a574a45cc17b139f1ff56a517f992e56
Dessaleng Ewart <binteldata@gmail.com>
2020-06-25 20:58:19 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: "Zenkichi", address: "new york", category: "japanese" )
+restaurant2 = Restaurant.create!(name: "china new star", address: "brooklyn", category: "chinese" )
+restaurant3 = Restaurant.create!(name: "belmondo", address: "berlin", category: "french" )
+restaurant4 = Restaurant.create!(name: "le marmiton", address: "lyon", category: "belgian" )
+restaurant5 = Restaurant.create!(name: "seta", address: "milan", category: "italian" )
+© 2020 GitHub, Inc.
